939b410c810f350eb2488b2823f0860b0bc9d7e5
Erik Allik <eallik@gmail.com>
2011-10-10 15:01:11 +0300

    Optimised Network tab 'Bridged Interfacs' grid column sizes

--- app/opennodeconsole/tabs/NetworkTab.js
+++ app/opennodeconsole/tabs/NetworkTab.js
@@ -11,20 +11,20 @@ Ext.define('opennodeconsole.tabs.NetworkTab', {
         this.items = [{
             xtype: 'gridpanel',
             title: 'Bridge Interfaces',
-
+            forceFit: true,
             store: rec.bridgeInterfaces(),
 
             columns: [
-                {header: 'Name', dataIndex: 'id', width: 45},
-                {header: 'Inet4', dataIndex: 'ipv4_address'},
-                {header: 'Inet6', dataIndex: 'ipv4_address'},
-                {header: 'Subnet Mask', dataIndex: 'subnet_mask'},
-                {header: 'Broadcast', dataIndex: 'bcast'},
+                {header: 'Name', dataIndex: 'id', width: 40},
+                {header: 'Inet4', dataIndex: 'ipv4_address', width: 75},
+                {header: 'Inet6', dataIndex: 'ipv6_address', width: 150},
+                {header: 'Subnet Mask', dataIndex: 'subnet_mask', width: 75},
+                {header: 'Broadcast', dataIndex: 'bcast', width: 75},
                 {header: 'Hardware Address', dataIndex: 'hw_address'},
-                {header: 'Metric', dataIndex: 'metric'},
-                {header: 'STP', dataIndex: 'stp'},
-                {header: 'RX', dataIndex: 'rx'},
-                {header: 'TX', dataIndex: 'tx'},
+                {header: 'Metric', dataIndex: 'metric', width: 25},
+                {header: 'STP', dataIndex: 'stp', width: 40},
+                {header: 'RX', dataIndex: 'rx', width: 50},
+                {header: 'TX', dataIndex: 'tx', width: 50},
             ]
         }];
 
