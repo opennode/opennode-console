fe276316323e0b485ef17ec709fa4c7edc22531e
Erik Allik <eallik@gmail.com>
2011-11-12 22:02:32 +0200

    Monkeypatch Ext.data.Association.create to handle our custom 'polymorphic' association type

--- app.js
+++ app.js
@@ -209,3 +209,27 @@ function inspectMethods(methodNames) {
         }
     });
 }
+
+Ext.data.Association.create = function(association){
+    if (!association.isAssociation) {
+        if (Ext.isString(association)) {
+            association = {
+                type: association
+            };
+        }
+
+        switch (association.type) {
+        case 'belongsTo':
+            return Ext.create('Ext.data.BelongsToAssociation', association);
+        case 'hasMany':
+            return Ext.create('Ext.data.HasManyAssociation', association);
+        case 'polymorphic':
+            return Ext.create('association.polymorphic', association);
+        default:
+            //<debug>
+            Ext.Error.raise('Unknown Association type: "' + association.type + '"');
+            //</debug>
+        }
+    }
+    return association;
+};
