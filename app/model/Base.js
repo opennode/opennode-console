ae4f9f4b6544f653550ec8a2c2e93f6a117573b3
Erik Allik <eallik@gmail.com>
2011-11-22 21:33:11 +0200

    Added Onc.model.Base.toString

--- app/model/Base.js
+++ app/model/Base.js
@@ -1,3 +1,11 @@
 Ext.define('Onc.model.Base', {
-    extend: 'Ext.data.Model'
+    extend: 'Ext.data.Model',
+
+    toString: function() {
+        return "<{0} {1}{2}>".format(
+            this.$className,
+            this.getId(),
+            this.getRepr ? (":" + this.getRepr()) : ""
+        );
+    }
 });
