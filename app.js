e4c86ee155291bfb0f4a2407efe6e5460c96482d
Erik Allik <eallik@gmail.com>
2011-12-04 18:12:14 +0200

    Added Ext.Base.cls helper

--- app.js
+++ app.js
@@ -42,7 +42,9 @@ Ext.application({
 Ext.override(Ext.Base, {
     toString: function() {
         return '<{0}>'.format(this.$className);
-    }
+    },
+
+    cls: function() { return this.$className.split('.').pop(); }
 });
 
 
