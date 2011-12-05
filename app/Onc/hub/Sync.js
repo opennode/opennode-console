4e6f4762b1d0ee6cb184a2733278dbfdeef8e694
Erik Allik <eallik@gmail.com>
2011-12-06 00:55:41 +0200

    Added polling for and syncing with object change events from /stream

--- app/Onc/hub/Sync.js
+++ app/Onc/hub/Sync.js
@@ -6,6 +6,9 @@ Ext.define('Onc.hub.Sync', {
         // The list of all records that have been registered with Sync
         _reg: [],
 
+        // Cache of all records by their URL
+        _byUrl: [],  // massoc
+
         // The record that is being synced with a record that was modified
         _doppelganger: null,
 
@@ -16,6 +19,11 @@ Ext.define('Onc.hub.Sync', {
                 console.warn("a new record was registered with Sync while syncing doppelgängers");
 
             this._reg.push(rec);
+
+            var url = rec.get('url');
+            if (url && this._byUrl.massoc(url).length === 0)
+                Onc.hub.Hub.subscribe(this._getHubListener(), [url]);
+            this._byUrl.setmassoc(url, rec);
         },
 
         recordModified: function(rec, changes) {
@@ -58,6 +66,57 @@ Ext.define('Onc.hub.Sync', {
 
         recordDestroyed: function(rec) {
             this._reg.delassoc(rec);
+        },
+
+        _syncRecord: function(rec, changes) {
+            if (this._doppelganger) {
+                if (this._doppelganger === rec)
+                    return;
+                else
+                    throw new Error("Attempt to sync a record while another one is already being synced");
+            }
+
+            this._doppelganger = rec;
+            try {
+                var wasEditing = rec.editing;
+
+                if (wasEditing)
+                    var unsavedChanges = clone(rec.getChanges());
+
+                rec.set(changes);
+                rec.commit();
+
+                if (wasEditing)
+                    rec.set(unsavedChanges);
+
+            } finally {
+                this._doppelganger = null;
+            }
+        },
+
+        _onDataFromHub: function(data) {
+            for (var url in data) {
+                var updates = data[url];
+
+                var attrChanges = {}
+                for (var i = 0; i < updates.length; i += 1) {
+                    var update = updates[i][1];
+                    if (update['event'] === 'change')
+                        attrChanges[update['name']] = update['value'];
+                    else
+                        console.warn("Unsupported update type %s", update['type']);
+                }
+                var records = this._byUrl.massoc(url);
+                records.forEach(function(rec) {
+                    this._syncRecord(rec, attrChanges);
+                }.bind(this));
+            }
+        },
+
+        _getHubListener: function() {
+            if (!this._hubListener)
+                this._hubListener = this._onDataFromHub.bind(this);
+            return this._hubListener;
         }
     }
 });
