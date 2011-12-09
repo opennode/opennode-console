opennode/opennode-console

commit ac1dfa234dd8d2ea50d5a84864cce7e5a59821cb

Redid 9131cff by moving presentation layer logic to the presentation layer

app/Onc/tabs/SystemTab.js
@@ -40,7 +40,8 @@ Ext.define('Onc.tabs.SystemTab', {
 
         var me = this;
         this._uptimeUpdateInterval = setInterval(function() {
-            me.down('#uptime').update('' + rec.getUptime());
+            me.down('#uptime').update(rec.get('status') === 'active' ?
+                                      rec.getUptime() : 'Server is switched off.');
         }, 1000);
 
         this.callParent(arguments);

app/model/Compute.js
@@ -52,7 +52,7 @@ Ext.define('Onc.model.Compute', {
 
     getUptime: function() {
         if (this.get('state') === 'inactive')
-            return 'Server is switched off.';
+            return null;
         var timestamp = new Date(Date.parse(this.get('startup_timestamp')));
 
         var s = Math.round((+(new Date()) - +timestamp) / 1000);

commit 9769cbc29caeab7d403c70b36b7b09d085ad14e1

Fixed a bug introduced in ac1dfa23

app/Onc/tabs/SystemTab.js
@@ -41,7 +41,7 @@ Ext.define('Onc.tabs.SystemTab', {
 
         var me = this;
         this._uptimeUpdateInterval = setInterval(function() {
-            me.down('#uptime').update(rec.get('status') === 'active' ?
+            me.down('#uptime').update(rec.get('state') === 'active' ?
                                       rec.getUptime() : 'Server is switched off.');
         }, 1000);
 