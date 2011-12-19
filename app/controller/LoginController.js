e783279b34ab7c9e6b425ca1786e3f415f64e9ab
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-12-19 14:17:36 +0100

    Fixed missing login window

--- app/controller/LoginController.js
+++ app/controller/LoginController.js
@@ -20,6 +20,7 @@ Ext.define('Onc.controller.LoginController', {
             },
             failure: function(response) {
                 console.assert(response.status === 403);
+                me.getView('LoginWindow').create();
             }
         });
 
