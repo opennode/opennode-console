55f1ccaa104fe9af18c3066d84080d9fb9373c4c
Erik Allik <eallik@gmail.com>
2011-12-20 21:16:03 +0200

    Automatically pop up login window when a 403 forbidden is detected

--- app/controller/LoginController.js
+++ app/controller/LoginController.js
@@ -6,10 +6,18 @@ Ext.define('Onc.controller.LoginController', {
     refs: [{ref: 'searchResults', selector: '#search-results'},
            {ref: 'tabs', selector: '#mainTabs'}],
 
+    _viewport: null,
+
     init: function() {
         var me = this;
 
         Ext.Ajax.withCredentials = true;
+
+        Ext.Ajax.on('requestexception', function() {
+            if (me._viewport)
+                me._viewport.destroy();
+            me.getView('LoginWindow').create();
+        });
 
         Ext.Ajax.request({
             url: BACKEND_PREFIX + 'auth',
@@ -35,6 +43,6 @@ Ext.define('Onc.controller.LoginController', {
     onAuth: function() {
         Onc.hub.Hub.run();
         Ext.getStore('ComputesStore').load();
-        this.getView('Viewport').create();
+        this._viewport = this.getView('Viewport').create();
     }
 });
