f5359f67825499e71b612ef56748e64e16bdcea4
Erik Allik <eallik@gmail.com>
2011-12-21 13:34:26 +0200

    Modified Onc.Backend to support calls from a Proxy

--- app/Onc/Backend.js
+++ app/Onc/Backend.js
@@ -7,34 +7,66 @@ Ext.define('Onc.Backend', {
         this.addEvents('loginrequired');
     },
 
-    request: function(method, url, opts) {
+    request: function(method, url, options, request) {
         var d = new Onc.util.Deferred;
 
-        opts = Ext.apply({}, opts);
+        options = options || {};
+        var successCodes = options.successCodes || [];
+        delete options.successCodes;
 
-        var _opts = {
+        // This is (primarily) to support Onc.backend.Proxy:
+        var callbackFn;
+        if (request && request.callback) {
+            callbackFn = request.callback;
+            delete request.callback;
+        } else {
+            callbackFn = options.callback || Ext.emptyFn;
+            delete options.callback;
+        }
+
+        var opts = {
             url: BACKEND_PREFIX + url,
             method: method,
             withCredentials: true,
-            success: function(response) {
-                var result = Ext.decode(response.responseText);
-                d.callback(result);
-            },
-            failure: function(response) {
-                if (response.status === 403) {
-                    this.fireEvent('loginrequired');
+
+            callback: function(_, success, response) {
+                var result;
+                try {
+                    result = Ext.decode(response.responseText);
+                } catch (ex) {
+                    console.error("Error parsing JSON response:");
+                    console.error(response.responseText);
+                }
+                if (!success) {
+                    if (response.status === 403 && !(403 in successCodes)) {
+                        this.fireEvent('loginrequired');
+                    } else {
+                        d.errback(result, response);
+                    }
                 } else {
                     var result = Ext.decode(response.responseText);
-                    d.errback(result);
+                    d.callback(result, response);
                 }
+
+                callbackFn.apply(window, arguments);
             }.bind(this)
         };
 
-        var keys = Ext.Object.getKeys(_opts);
-        console.assert(keys.every(function(k) { return !(k in opts); }),
+        var keys = Ext.Object.getKeys(opts);
+        console.assert(keys.every(function(k) { return !(k in options); }),
                        "Request options shouldn't specify any of {0}".format(keys.join(', ')));
+        Ext.apply(options, opts);
 
-        Ext.Ajax.request(Ext.apply(_opts, opts));
+        if (request) {
+            for (var key in options) {
+                if (request.hasOwnProperty(key))
+                    console.warn("'%s' property of request will be overwritten", key);
+            }
+            Ext.apply(request, options);
+            Ext.Ajax.request(request);
+        } else {
+            Ext.Ajax.request(options);
+        }
 
         return d;
     }
