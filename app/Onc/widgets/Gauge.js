3dfac26c43413b8ce393b59cc68571d582d72890
Erik Allik <eallik@gmail.com>
2011-12-28 15:25:02 +0200

    Changed the logic in Guage.refresh to use fractional as opposed to percentage ratios

--- app/Onc/widgets/Gauge.js
+++ app/Onc/widgets/Gauge.js
@@ -10,8 +10,8 @@ Ext.define('Onc.widgets.Gauge', {
     value: 0.0,
     max: 1.0,
     unit: null,
-    criticalLevel: 0,
-    criticalCurve: 5,
+    criticalLevel: 0.00,
+    criticalCurve: 0.05,
 
     tpl: new Ext.XTemplate(
         '<span>',
@@ -45,22 +45,24 @@ Ext.define('Onc.widgets.Gauge', {
 
     refresh: function() {
         if (this.rendered) {
-            var ratio = Math.round(this.value / this.max * 100);
+            var ratio = (this.value / this.max).round(2);
             ratio = (isNaN(ratio) ? 0 : ratio);
-            if (ratio > 100) {
-                ratio = 100;
+
+            if (ratio > 1.0) {
+                ratio = 1.0;
                 this.el.down('.bar').addCls('overload');
             } else {
                 this.el.down('.bar').removeCls('overload');
             }
 
-            this.el.down('.value').update('' + ratio);
-            this.el.down('.bar div').setWidth('' + ratio + '%');
+            var ratioPercentage = (ratio * 100).round();
+            this.el.down('.value').update('' + ratioPercentage);
+            this.el.down('.bar div').setWidth('' + ratioPercentage + '%');
 
-            var redMax = 100 - this.criticalLevel;
+            var redMax = 1.0 - this.criticalLevel;
             var redLevel = ratio - this.criticalLevel;
             if (redLevel > 0) {
-                var redFactor = Math.pow(redLevel / redMax, this.criticalCurve) * 255;
+                var redFactor = Math.pow(redLevel / redMax, 100 * this.criticalCurve) * 255;
                 var color = new Ext.draw.Color(redFactor, 0, 255 - redFactor);
                 this.el.down('.bar div').applyStyles({'background-color': color});
             }
