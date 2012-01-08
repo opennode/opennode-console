36da74e5c8d175698dafea84b8fa172f5af03045
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-01-08 11:11:40 +0100

    Ignored jenkins tags for computing version

--- version.py
+++ version.py
@@ -38,7 +38,7 @@ from subprocess import Popen, PIPE
 
 def call_git_describe(abbrev=4):
     try:
-        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
+        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev, '--match', '[0-9]*'],
                   stdout=PIPE, stderr=PIPE)
         p.stderr.close()
         line = p.stdout.readlines()[0]
