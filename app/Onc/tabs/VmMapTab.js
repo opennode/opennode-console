491e216cf556a6cbf618a18c68d0d93b725ee780
anthrax11 <andres.traks@gmail.com>
2012-01-23 03:31:28 +0200

    Created HTML&CSS-based VM map layout.

--- app/Onc/tabs/VmMapTab.js
+++ app/Onc/tabs/VmMapTab.js
@@ -8,16 +8,47 @@ Ext.define('Onc.tabs.VmMapTab', {
         this.items = [{
             xtype: 'gridpanel',
             hideHeaders: true,
+            columnLines: true,
             store: 'PhysicalComputesStore',
-            //plugins: Ext.create(''),
+
             columns: [
                 {header: 'Name', dataIndex: 'hostname', width: 100},
-                {header: 'Map', flex: 1},
+                //{header: 'Disk pool size', dataIndex: 'diskspace', width: 15},
+                {header: 'Map', dataIndex: 'id', flex: 1,
+                    renderer: function(value, meta, rec) {
+                        var totalMemory = rec.get('memory_usage'),
+                            freeMemory = totalMemory,
+                            vms = rec.getChild('vms').children(),
+                            vm_list = "";
 
-                {header: 'ID', dataIndex: 'id', width: 130, hidden: true}
+                        vms.each( function(vm) {
+                            var memory = vm.get('memory_usage'),
+                                width = parseInt(100 * (memory / totalMemory));
+                            freeMemory -= memory;
+                            vm_list += ["<td width=\"" + width + "%\">",
+                                "<div class=\"vmmap-name\">" + vm.get('hostname') + "</div>",
+                                "<div class=\"vmmap-mem\">" + parseInt(memory) + "</div>",
+                                "<div class=\"vmmap-uptime\">" + vm.getUptime() + "</div>",
+                                "<div class=\"vmmap-num-cores\">" + vm.get('num_cores') + "</div>",
+                                "</td>"].join('\n');
+                        });
+
+                        if (freeMemory) {
+                            var width = parseInt(100 * (freeMemory / totalMemory));
+                            vm_list += ["<td width=\"" + width + "%\">",
+                                "<div class=\"vmmap-name\">free</div>",
+                                "<div class=\"vmmap-mem\">" + parseInt(freeMemory) + "</div>",
+                                "</td>"].join('\n');
+                        }
+
+                        return ["<table width=\"100%\"><tbody><tr>",
+                        vm_list,
+                        "</tr></tbody></table>"].join('\n');
+                    }
+                }
             ]
         }];
-        
+
         this.callParent(arguments);
     }
 });
