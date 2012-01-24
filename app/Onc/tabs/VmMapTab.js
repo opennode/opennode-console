760c7e6a561b2c16ba220875d44bc2dddab1fe6a
anthrax11 <andres.traks@gmail.com>
2012-01-24 20:36:00 +0200

    Show IP address in VM map and enable horizontal scrolling. Clean up CSS formatting.

--- app/Onc/tabs/VmMapTab.js
+++ app/Onc/tabs/VmMapTab.js
@@ -9,24 +9,25 @@ Ext.define('Onc.tabs.VmMapTab', {
             xtype: 'gridpanel',
             hideHeaders: true,
             columnLines: true,
+            id: 'vmmap',
             store: 'PhysicalComputesStore',
 
             columns: [
                 {header: 'Name', dataIndex: 'hostname', width: 100},
                 //{header: 'Disk pool size', dataIndex: 'diskspace', width: 15},
-                {header: 'Map', dataIndex: 'id', flex: 1,
-                    renderer: function(value, meta, rec) {
-                        var totalMemory = rec.get('memory_usage'),
-                            freeMemory = totalMemory,
+                {header: 'Map', dataIndex: 'memory_usage', flex: 1,
+                    renderer: function(totalMemory, meta, rec) {
+                        var freeMemory = totalMemory,
                             vms = rec.getChild('vms').children(),
                             vm_list = "";
 
                         vms.each( function(vm) {
-                            var memory = vm.get('memory_usage'),
+                            var memory = vm.get('memory'),
                                 width = parseInt(100 * (memory / totalMemory));
                             freeMemory -= memory;
                             vm_list += ["<td class=\"node-cell\" width=\"" + width + "%\">",
                                 "<div class=\"name\">" + vm.get('hostname') + "</div>",
+                                "<div class=\"name\">" + vm.get('ipv4_address') + "</div>",
                                 "<div class=\"mem\">" + parseInt(memory) + "</div>",
                                 "<span class=\"uptime\">" + vm.getUptime() + "</span>",
                                 "<span class=\"cores\">" + vm.get('num_cores') + "</span>",
@@ -41,7 +42,7 @@ Ext.define('Onc.tabs.VmMapTab', {
                                 "</td>"].join('\n');
                         }
 
-                        return ["<table id=\"vmmap\" width=\"100%\"><tbody><tr>",
+                        return ["<table width=\"100%\"><tbody><tr>",
                         vm_list,
                         "</tr></tbody></table>"].join('\n');
                     }
