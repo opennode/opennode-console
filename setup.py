094409f292698a715082d8e70e953cc2d0b5c2ec
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-03-05 16:07:54 +0100

    Fixed setup.py so that it includes all necessary package data

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
+import os
+
 from setuptools import setup, find_packages
 from version import get_git_version
 
+
+def files(dir, exclude=[]):
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            if not any(("%s/%s" % (root, f)).startswith(e) for e in exclude):
+                yield "../../%s/%s" % (root, f)
+
+def all_files(*dirs, **kwargs):
+    exclude = kwargs.get('exclude', [])
+    for d in dirs:
+        for f in files(d, exclude):
+            yield f
+
+package_data = {'opennode.onc': list(all_files('app', 'css', 'ext-4.0', 'novnc', 'term',
+                                               exclude=('ext-4.0/docs', 'ext-4.0/builds', 'ext-4.0/build',
+                                                        'ext-4.0/jsbuilder', 'ext-4.0/welcome', 'ext-4.0/examples')))}
 
 setup(
     name = "opennode.oms.onc",
@@ -9,7 +27,7 @@ setup(
     author = "OpenNode Developers",
     author_email = "developers@opennodecloud.com",
     packages = find_packages(),
-    package_data={'opennode.onc': ['../../index.html', '../../app/**/*.*']},
+    package_data=package_data,
     namespace_packages = ['opennode'],
     zip_safe=False, # we need to serve real files
     entry_points = {'oms.plugins': ['onc = opennode.onc.main:ONCPlugin']},
