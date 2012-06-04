49b7ce45c8509dae419cc91c3488bca2a3d499fb
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-06-04 18:20:03 +0200

    Removed logging from VM state change code (ON-201)

--- app/controller/ComputeController.js
+++ app/controller/ComputeController.js
@@ -45,7 +45,6 @@ Ext.define('Onc.controller.ComputeController', {
         var observers = [];
 
         var timerId = setTimeout(function() {
-            console.log('* timedout');
             // signal connected observers that we are done
             Ext.each(observers, function(observer) {
                 observer.finished();
@@ -55,22 +54,18 @@ Ext.define('Onc.controller.ComputeController', {
 
         function checkCompleted() {
             if(observers.length == 0) {
-                console.log('* no more computes to wait for');
                 clearTimeout(timerId);
                 callback();
             }
         }
 
         function onVMStateChanged(observer) {
-            console.log('# enter onVMStateChanged', observer);
             observers.remove(observer);
             checkCompleted()
         }
 
         Ext.each(vms, function(vm) {
-            if(vm.get('state') === desiredState) {
-                console.log(vm.get('hostname') + ': compute already in ' + desiredState + ' state - skipping');
-            } else {
+            if(vm.get('state') != desiredState) {
                 var vmObserver = new this._createObserver(vm, desiredState, onVMStateChanged);
                 observers.push(vmObserver);
                 vmObserver.changeState();
@@ -85,29 +80,21 @@ Ext.define('Onc.controller.ComputeController', {
             changeState: function() {
                 vm.set('state', desiredState);
                 vm.save();
-                this.log('start state change to ' + desiredState);
+
                 Onc.hub.Hub.subscribe(this.onDataFromHub, {'compute': vm.get('url')}, 'state_change');
-                this.log('subscribed');
             },
 
             onDataFromHub: function(values) {
                 values.compute.forEach(function(el) {
                     var eo = el[1];
-                    this.log ('  event -- ' + eo.event + ', ' + eo.name + ', ' + eo.old_value + ', ' + eo.value);
-                    if(eo.name === 'effective_state' && eo.value === desiredState){
-                        this.log('state changed to ' + desiredState);
+                    if(eo.name === 'effective_state' && eo.value === desiredState)
                         this.finished();
-                    };
                 }, this);
             }.bind(this),
 
             finished: function() {
                 Onc.hub.Hub.unsubscribe(this.onDataFromHub);
                 vmStateChangedCallback(this);
-            }
-
-            log: function(msg) {
-                console.log('  ' + vm.get('hostname') + ': ' + msg);
             }
         };
     }
