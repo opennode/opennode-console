d0a70cf0bf18102f84a31f7ee0821c3f880a6c9d
Andrej Kolic <andrej.kolic@gmail.com>
2012-07-04 12:21:53 +0200

    EventBus component created (ON-515)

---
+++ app/Onc/EventBus.js
@@ -0,0 +1,36 @@
+Ext.define('Onc.EventBus', {
+    extend: 'Ext.util.Observable',
+    singleton: true
+});
+
+
+Ext.app.Controller.override({
+    constructor: function(config){
+        var instance = this.callOverridden(arguments);
+
+        // register listeners as defined in busListeners property
+        // example:
+        //  busListeners: {
+        //      event1: function(...){...},
+        //      event2: function(...){...}
+        //  },
+
+        this.registerBusListeners(this.busListeners);
+        return instance;
+    }
+});
+
+
+Ext.app.Controller.implement({
+    fireBusEvent: function(eventName, args){
+        Onc.EventBus.fireEvent(eventName, args);
+    },
+
+    registerBusListeners: function(listeners){
+        if(listeners !== undefined){
+            Ext.Object.each(listeners, function(ev, listener){
+                Onc.EventBus.addListener(ev, listener, this);
+            });
+        };
+    },
+});
