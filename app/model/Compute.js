1fe68e9bb8cf0a1574879ce00d34000b7e804f20
Ilja Livenson <ilja.livenson@gmail.com>
2012-07-06 10:35:02 +0200

    Added loadParent function for retrieving HN of a VM (ON-513)

--- app/model/Compute.js
+++ app/model/Compute.js
@@ -117,5 +117,14 @@ Ext.define('Onc.model.Compute', {
                 return false;
         };
         return true;
+    },
+
+    loadParent: function(successCb, failureCb) {
+        // we assume that physical computes don't have any parents
+        if (!this.isPhysical()) {
+            var computeStore = Ext.data.StoreManager.lookup('ComputesStore');
+            var parentId = this.get('url').split('/')[2];
+            computeStore.loadById(parentId, successCb, failureCb);
+        }
     }
 });
