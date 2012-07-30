02b2cd2a59552d78bfa288017b2c4a83d7c85232
Ilja Livenson <ilja.livenson@gmail.com>
2012-07-31 01:54:17 +0300

    Modified test-harness to load ONC through the Ext.Loader (ON-17)

--- test-harness.js
+++ test-harness.js
@@ -2,14 +2,22 @@ var Harness = Siesta.Harness.Browser.ExtJS;
 
 Harness.configure({
     title: 'ONC Test Suite',
-    loaderPath:
-    {
-        'Onc' : 'app',
-    },
 
     preload: [
         "ext-4.1/resources/css/ext-all.css",
         "ext-4.1/ext-all.js",
+        {   text: "Ext.Loader.setConfig('paths', {" +
+                  "'Ext': 'ext-4.1/src'," +
+                  "'Onc': './app/Onc'," +
+                  "'Ext.ux': 'ext-4.1/examples/ux' }); " + 
+                  "Ext.Loader.setConfig({ enabled : true})"
+        },
+        "log4js/sm/log/log4js-ext-all.js",
+        "support.js",
+        "util.js",
+        "conf-default.js",
+        "config.js",
+        "app.js"
     ]
 });
 
