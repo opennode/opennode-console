d166b750ef74c9db9e8c96fe26d13873b1054037
Ilja Livenson <ilja.livenson@gmail.com>
2013-01-24 14:18:07 +0200

    Fixed conflicting class name, code cleanup

--- app/controller/DashboardController.js
+++ app/controller/DashboardController.js
@@ -1,28 +1,28 @@
-Ext.define('Onc.controller.MainController', {
-	extend: 'Ext.app.Controller',
-	
-	stores: ['ComputesStore', 'PhysicalComputesStore'],
-	busListeners: {
-		getRunningServices: function(){
-			this._getRunningServices();
-		} 
-	},
-	
-    _getRunningServices: function(){
-    	var url = '/proc';
-    	Onc.core.Backend.request('PUT', url, {
-    		success: function(response) {
-    			this._load();
-    		}.bind(this),
-    		failure: function(response) {
-    			console.error('Request failed: ' + response.responseText);
-    		}
-    	});
-    	
+Ext.define('Onc.controller.DashboardController', {
+    extend: 'Ext.app.Controller',
+
+    stores: ['ComputesStore', 'PhysicalComputesStore'],
+
+    busListeners: {
+        getRunningServices: function(){
+            this._getRunningServices();
+        }
     },
-	_getPendingActions: function() {},
-	_getRunningTasks: function() {},
-	_getLastestEvents: function() {}
-	
-	
+
+    _getRunningServices: function(){
+        var url = '/proc';
+        Onc.core.Backend.request('PUT', url, {
+           success: function(response) {
+            this._load();
+           }.bind(this),
+            failure: function(response) {
+                console.error('Request failed: ' + response.responseText);
+        }
+        });
+    },
+
+    _getPendingActions: function() {},
+    _getRunningTasks: function() {},
+    _getLastestEvents: function() {}
+
 });
