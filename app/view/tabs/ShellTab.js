ed91792cff9e1468dfc26980e23d62d62338f5d4
Ilja Livenson <ilja.livenson@gmail.com>
2013-01-29 21:47:59 +0200

    Reverted removal of ShellTab as it is used also for displaying shell of the VMs and HNs

---
+++ app/view/tabs/ShellTab.js
@@ -0,0 +1,16 @@
+Ext.define('Onc.view.tabs.ShellTab', {
+        extend: 'Onc.view.tabs.Tab',
+    alias: 'widget.computeshelltab',
+
+    layout: 'fit',
+    bodyPadding: 0,
+
+    initComponent: function() {
+                this.items = [{
+                                xtype: 'shell',
+            url: (this.shellConfig || {}).url
+            }];
+                        this.callParent(arguments);
+                            }
+});
+
