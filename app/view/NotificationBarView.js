a7e7d586cbfe15fb8e886a760d0a8b85f4a186b9
andriusj <asdasd123>
2013-02-14 15:01:21 +0200

    ITACLOUD-87: Removed redundant code

--- app/view/NotificationBarView.js
+++ app/view/NotificationBarView.js
@@ -2,67 +2,10 @@ Ext.define('Onc.view.NotificationBarView', {
     extend: 'Ext.Component',
     alias: 'widget.notificationBar',
 
-    renderTo: Ext.getBody(),
-    renderTpl: [
-        '<div class="notificationbar">',
-        '<p>{desc}</p>',
-        '</div>'
-    ],
-    renderData: {
-        desc: "..."
-    },
-    renderSelectors: {
-        descEl: 'p'
-    },
-
-    listeners: {
-        afterrender: function(cmp){
-            cmp.descEl.addListener('click', function(){
-                this.hideMessage();
-            }, this);
-        }
-    },
-
     hidden: true,
-
-    _timerId: null,
-
 
     displayMessage: function(messageText, messageType){
         Ext.example.msg('', messageText);
-//        this._clearTimer();
-//        this._setMessage(messageText, messageType);
-//        this._setTimer();
-//        this.show();
-    },
-
-    hideMessage: function(){
-        this._clearTimer();
-        this.hide();
-    },
-
-
-    _setMessage: function(messageText, messageType){
-        this.descEl.dom.innerHTML = messageText;
-        this.removeCls(['notificationbar-info', 'notificationbar-error']);
-        if(messageType === 'error'){
-            this.addCls('notificationbar-error');
-        }
-        else {
-            this.addCls('notificationbar-info');
-        }
-    },
-
-    _clearTimer: function(){
-        if(this._timerId){
-            clearTimeout(this._timerId);
-            this._timerId = null;
-        }
-    },
-
-    _setTimer: function(){
-        this._timerId = setTimeout(function() {
-            this.hideMessage();
-        }.bind(this), 10000);
     }
+
 });
