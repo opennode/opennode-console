37fd32e6af2df4c3c800cd7e937ef87f52797702
Maigo Erit <maigo.erit@gmail.com>
2013-03-21 21:34:59 +0200

    Fix: TemplatesStore wasn't loaded correctly. Every win open fresh load. (OMS-274)

--- app/view/compute/NewVmView.js
+++ app/view/compute/NewVmView.js
@@ -176,6 +176,9 @@ Ext.define('Onc.view.compute.NewVmView', {
 
     initComponent: function() {
 
+        //So window open fresh data is loaded afterwards
+        Ext.getStore('TemplatesStore').removeAll();
+        
         this.items = {
             xtype: 'form',
             items: [
@@ -319,8 +322,18 @@ Ext.define('Onc.view.compute.NewVmView', {
                                             });
                                         }
                                     } else {
-                                        this.loadTemplates(Ext.getStore('TemplatesStore').load());
-                                        this.parentCompute = null;
+                                        var store = Ext.getStore('TemplatesStore');
+                                        if (store.getTotalCount() == 0) {//dont load agein when switching between alloc policy items
+                                            store.load({
+                                                scope: this,
+                                                callback: function(records, operation, success) {
+                                                    if (success) {
+                                                        this.loadTemplates(Ext.getStore('TemplatesStore'));
+                                                        this.parentCompute = null;
+                                                    }
+                                                }
+                                            });
+                                        }
                                     }
                                 }.bind(this),
                                 afterrender: function(combo, eOpts) {
