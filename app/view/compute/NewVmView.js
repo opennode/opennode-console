b9486ca690286a7c031bba91786ffb7fac09aece
Maigo Erit <maigo.erit@gmail.com>
2013-03-21 23:46:25 +0200

    Grammar fix (OMS-276)

--- app/view/compute/NewVmView.js
+++ app/view/compute/NewVmView.js
@@ -328,7 +328,7 @@ Ext.define('Onc.view.compute.NewVmView', {
                                         }
                                     } else {
                                         var store = Ext.getStore('TemplatesStore');
-                                        if (store.getCount() == 0) {//dont load agein when switching between alloc policy items
+                                        if (store.getCount() == 0) { // don't load again when switching between alloc policy items
                                             store.load({
                                                 scope: this,
                                                 callback: function(records, operation, success) {
