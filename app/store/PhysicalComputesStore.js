37551c3867fabea7b3e98680e2ddd2e847a2fd60
Maigo Erit <maigo.erit@gmail.com>
2013-04-22 09:16:12 +0300

    vms-openvz registering with HUB in separate store. (OMS-347)

--- app/store/PhysicalComputesStore.js
+++ app/store/PhysicalComputesStore.js
@@ -2,8 +2,6 @@ Ext.define('Onc.store.PhysicalComputesStore', {
     extend: 'Ext.data.Store',
     model: 'Onc.model.Compute',
 
-    autoLoad: true,// important: so /machines/{computeID}/vms-openvz/ is registered with HUB and events like remove are received
-
     proxy: {
         type: 'onc',
         reader: {
