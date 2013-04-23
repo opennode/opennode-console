aaa831f3f331d9275ffdd39d00d04992d90b56e8
Maigo Erit <maigo.erit@gmail.com>
2013-04-23 09:47:22 +0300

    Excluded openVZ with the query.(OMS-291)

--- app/store/VmGridStore.js
+++ app/store/VmGridStore.js
@@ -1,33 +1,23 @@
 Ext.define('Onc.store.VmGridStore', {
     extend: 'Onc.core.Store',
     model: 'Onc.model.Compute',
-    pageSize:10,
+    pageSize: 10,
     proxy: {
         type: 'onc',
         reader: {
             type: 'json',
             root: 'children',
-            totalProperty:'totalChildren'
+            totalProperty: 'totalChildren'
         },
         extraParams: {
             'depth': 1,
             'attrs': 'state,hostname,ipv4_address,cpu_info,url,num_cores,swap_size,diskspace,memory,suspicious,tags,features',
+            'exclude': 'openvz'
         },
         limitParam: 'limit',
         pageParam: 'offset',
         startParam: null,
         url: 'computes'
     },
-    // This does not work on filtered grid
-    // filters: [{
-    // property: 'id',
-    // value: /[^openvz]/
-    // }],
-    //    
-    listeners: {
-        load: function(store, records, successful, eOpts) {
-            // remove manually unwanted record, filtering does not work on filtered grid
-            store.remove(store.findRecord("id", "openvz"));
-        }
-    },
+
 });
