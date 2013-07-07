ecfc80c6bfb2d2f8c998233ea98591e59e812893
Ilja Livenson <ilja.livenson@gmail.com>
2013-07-07 23:18:03 +0300

    Modified setup.py to pack correctly built production ONC (OMS-48)

--- setup.py
+++ setup.py
@@ -4,37 +4,49 @@ from setuptools import setup, find_packages
 from version import get_git_version
 
 
-def files(dir, exclude=[]):
-    for root, dirs, files in os.walk(dir):
+def files(folder, exclude=[]):
+    found_files = []
+    for root, dirs, files in os.walk(folder):
         for f in files:
             if not any(("%s/%s" % (root, f)).startswith(e) for e in exclude):
-                yield "../../%s/%s" % (root, f)
+                found_files.append((root, f))
+    return found_files
 
-def all_files(*dirs, **kwargs):
+
+def flatten_all_files(*dirs, **kwargs):
     exclude = kwargs.get('exclude', [])
+    root = kwargs.get('root', '')
+    all_files = []
     for d in dirs:
         for f in files(d, exclude):
-            yield f
+            froot, fnm = f
+            prefix_start = froot.find(root)
+            assert prefix_start > -1, 'Impossible base root provided. Found files do not match: %s' % root
+            all_files.append(("%s" % (froot[prefix_start + len(root):]), ["%s/%s" % (froot, fnm)]))
+    return all_files
 
-package_data = {'opennode.onc': list(all_files('app', 'css', 'theme', 'lib/ext-4.1', 'lib/novnc', 'lib/term', 'lib/log4js', 'img',
-                                               exclude=('lib/ext-4.1/docs', 'lib/ext-4.1/builds', 'lib/ext-4.1/build',
-                                                        'lib/ext-4.1/jsbuilder', 'lib/ext-4.1/welcome')))
-                + ['../../index.html', '../../favicon.ico', '../../*.js', '../../beep.wav', '../../portal.html']}
+#
+data_files = flatten_all_files('build/Onc/production/',
+                    exclude=('build/Onc/production/lib/ext-4.2', 'build/Onc/production/.sass-cache'),
+                    root='build/Onc/production/'
+              )
+data_files.append(('', ['favicon.png', 'beep.wav', 'portal.html']))
 
 setup(
-    name = "opennode.oms.onc",
-    version = get_git_version(),
-    description = """Open Node Client""",
-    author = "OpenNode Developers",
-    author_email = "developers@opennodecloud.com",
-    packages = find_packages(),
-    package_data=package_data,
-    namespace_packages = ['opennode'],
-    zip_safe=False, # we need to serve real files
-    entry_points = {'oms.plugins': ['onc = opennode.onc.main:OncPlugin']},
-    install_requires = [
-        "setuptools", # Redundant but removes a warning
+    name="opennode.oms.onc",
+    version=get_git_version(),
+    description="""OpenNode Console application""",
+    author="OpenNode Developers",
+    author_email="info@opennodecloud.com",
+    packages=find_packages(),
+    data_files=data_files,
+    namespace_packages=['opennode'],
+    zip_safe=False,  # we need to serve real files
+    entry_points={'oms.plugins': ['onc = opennode.onc.main:OncPlugin']},
+    install_requires=[
+        "setuptools",  # Redundant but removes a warning
         "opennode.oms.core",
         "opennode.oms.knot",
         ],
+    license='GPLv2',
 )
